43190faf905387ed1d3e29652396f5142f4ebfe4
Sebastian Nitu <me@sebnitu.com>
2012-06-09 12:37:43 -0700

    Fixed some bugs and added some clearfixes to styles

--- bootstrap.js
+++ bootstrap.js
@@ -30,6 +30,8 @@ document.write(unescape('%3Clink rel="stylesheet" media="all" href="' + myScript
 !window.jQuery && document.write(unescape('%3Cscript src="' + myScriptSrc + 'bd.jquery.min.js"%3E%3C/script%3E'));
 // Included baseDemo JavaScript
 document.write(unescape('%3Cscript src="' + myScriptSrc + 'bd.ui.js"%3E%3C/script%3E'));
+// Mobile viewport optimized
+document.write(unescape('%3Cmeta name="viewport" content="width=device-width, initial-scale=1.0"%3E'));
 
 /*-------------------------------------------
 	Fin
